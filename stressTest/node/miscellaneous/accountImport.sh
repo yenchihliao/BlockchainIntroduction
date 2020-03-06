f49080d258fc20263d5b3bcb2ed3008879ce481c
yenchihliao <yen.yenchih.liao@gmail.com>
2020-03-06 07:15:19 +0000

    import accounts to geth node

--- stressTest/node/miscellaneous/accountImport.sh
+++ stressTest/node/miscellaneous/accountImport.sh
@@ -1,4 +1,5 @@
 for((i=1 ; i <= 300; i++)){
 	sed -n "${i}p" ./priv.txt > tmp
-	geth --nousb account import --datadir ethdata --password pwd tmp
+	geth --nousb account import --datadir ../ethdata --password ../pwd tmp
 }
+rm tmp
